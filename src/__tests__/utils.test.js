228df807830135d0a12ff00f4e6cc3e26c3c8eef
Riko <riko.fluchel@gmail.com>
2025-04-19 10:08:40 -0400

    complete lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,30 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+    it("indicates racecar is a palindrome", () => {
+        const word = "racecar";
+        const isRaceCarPalindrome = isPalindrome(word);
+
+        expect(isRaceCarPalindrome).toBe(true);
+    });
+
+    it("indicates whether car is a palindrome", () => {
+        const word = "car";
+        const isCarPalindrome = isPalindrome(word);
+
+        expect(isCarPalindrome).toBe(false);
+    });
+
+    it("returns false for an empty string", () => {
+        const word = "";
+        const isEmptyStringPalindrome = isPalindrome(word);
+        expect(isEmptyStringPalindrome).toBe(false);
+    });
+
+    it("returns true for words that are combination of uppercase and lowercase letters", () => {
+        const word = "rAcEcAr";
+        const isRaceCarPalindrome = isPalindrome(word);
+        expect(isRaceCarPalindrome).toBe(true)
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,14 @@
 // Your code here
+
+function reverse(string) {
+    return string.split("").reverse().join("");
+}
+
+export function isPalindrome(word) {
+
+    if (word === ""){
+        return false
+    }
+    const lowerCaseWord = word.toLowerCase();
+    return lowerCaseWord === reverse(lowerCaseWord);
+}
